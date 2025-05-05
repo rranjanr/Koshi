25ead40f269e93fc597b4115f24767a61844f41c
Rahul Ranjan <RAHUL1.777402@mmamc.tu.edu.np>
2025-05-05 11:12:37 +0545

    inital

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo '<h1>Welcome to Koshi Biratnagar!</h1>';
+echo '<div style="text-align: center; font-family: Arial, sans-serif; margin-top: 50px;">';
+echo '<h1 style="color: #2c3e50;">Welcome to Koshi Pardesh!</h1>';
+echo '<img src="koshi.jpg" style="width: 80%; max-width: 1024px; border: 2px solid #2c3e50; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">';
+echo '</div>';
+?>
